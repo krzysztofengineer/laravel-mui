55f79ff5301daa5efbbda997c520046540084d86
Krzysztof <krzysztof@engineer.com>
2025-01-18 14:25:10 +0100

    add mui theme

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -5,10 +5,23 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 import { createInertiaApp } from '@inertiajs/react';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { grey } from '@mui/material/colors';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            light: grey[900],
+            main: grey[900],
+            dark: grey[900],
+            contrastText: grey[100],
+        },
+    }
+})
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -20,7 +33,9 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(<ThemeProvider theme={theme}>
+            <App {...props} />
+        </ThemeProvider>);
     },
     progress: {
         color: '#4B5563',
